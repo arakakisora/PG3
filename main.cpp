f9feda99b63279d457e94c7f8fd94b8cab9dad4c
arakakisora <k023g0045@g.neec.ac.jp>
2024-10-16 10:55:33 +0900

    確認課題

--- main.cpp
+++ main.cpp
@@ -1,8 +1,51 @@
-#include "stdio.h"
+#include <stdio.h>
+#include <stdlib.h>  
+#include <time.h>    
+#include <windows.h> 
+
+
+int rollDice() {
+    return rand() % 6 + 1;
+}
+
+
+void showResult(int* result) {
+    if (*result == 1) {
+        printf("正解\n");  
+    }
+    else {
+        printf("不正解\n");  
+    }
+}
+
+
+void setTimeout(void (*pFunc)(int*), int seconds, int* result) {
+    Sleep(seconds * 1000);  
+    pFunc(result);          
+}
+
 int main() {
+    srand(time(NULL));  
 
-	printf("arakaki_sora\n");
-	printf("アラカキソラ");
+    int num, dice, ans;
+    printf("サイコロの出目が奇数なら1を、偶数なら2を入力してね！: ");
+    scanf_s("%d", &num);
 
+    dice = rollDice();
+    printf("何の目が出るかな？\n");
+ 
 
+   
+    if ((dice % 2 == 0 && num == 2) || (dice % 2 != 0 && num == 1)) {
+        ans = 1;  
+    }
+    else {
+        ans = 0;  
+    }
+
+    
+    setTimeout(showResult, 3, &ans);
+    printf("出た目は%dだよ", dice);
+
+    return 0;
 }
