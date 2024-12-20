b4acdfd8258ff1858de851c3b2ee01b1d7e65c43
arakakisora <k023g0045@g.neec.ac.jp>
2024-12-20 22:32:05 +0900

    確認課題

--- main.cpp
+++ main.cpp
@@ -1,8 +1,24 @@
 #include "stdio.h"
+#include <thread>
+#include <mutex>
+#include <cstdio>
+
+void print_message(const char* message) {
+
+	printf("%s\n", message);
+}
+
 int main() {
 
-	printf("arakaki_sora\n");
-	printf("アラカキソラ");
+	
+	std::thread t1(print_message, "thread1");
+	t1.join();
+	std::thread t2(print_message, "thread2");
+	t2.join();
+	std::thread t3(print_message, "thread3");
+	t3.join();
+
+	return 0;
 
 
 }
